5419c85b3a759bf7aa3bc5ffd9b43968dbc1984a
paldy <58873376+paldy@users.noreply.github.com>
2022-11-15 18:44:06 +0100

    Add files via upload

---
+++ output.pl
@@ -0,0 +1,11 @@
+
+print "Some beautiful text is here: and, some ... else; here as Well.\n";
+$foo =  5678;
+$bar =  23.17;
+print "$bar\n";
+$anystring =  'Some wondering text with any quotes and newlines';
+@foolist =  (23,'sdf cv',"bnm vb");
+%anyhashbar =  ('22', 34, '45d', 'xcvb');
+$sumfoobar =  $foo + $bar;
+print "$sumfoobar\n";
+print "Some beautiful text about $bar and $foo for instance.\n";
